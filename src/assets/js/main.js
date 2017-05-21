4151f292d1875826b30e929fa854fad4f4ebdbea
Evgeniy28 <azarov91@mail.ru>
2017-05-21 17:33:49 +0300

    add modal forms, falidation

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,3 @@
-console.log('Hello World!');
-
 $(document).ready(function() {
   // Go section recomendation
   $('.go-info').on('click', function(e) {
@@ -23,5 +21,91 @@ $(document).ready(function() {
       scrollTop: $('.header').offset().top
     }, 1500);
     e.preventDefault();
+  });
+
+  // Open Modal Form
+  function removeError() {
+    if ($('.modal-form').hasClass('modal-error')) {
+      $('.modal-form').removeClass('modal-error');
+    }
+  };
+
+  $('.call-me').on('click', function(e) {
+    e.preventDefault();
+    removeError();
+    $('.modal').addClass('is-active');
+    $('.form-for-me').addClass('is-form-show');
+  });
+
+  $('.call-friend').on('click', function(e) {
+    e.preventDefault();
+    removeError();
+    $('.modal').addClass('is-active');
+    $('.form-for-friend').addClass('is-form-show');
+  });
+
+  // Close Modal Form button
+  $('.modal-close').on('click', function(e) {
+    e.preventDefault();
+    if ($('.modal').hasClass('is-active')) {
+      $('.modal').removeClass('is-active');
+      $('.form-for-me').removeClass('is-form-show');
+      $('.form-for-friend').removeClass('is-form-show');
+      $('.form-for-me input').removeClass('is-danger');
+      $('.form-for-friend input').removeClass('is-danger');
+    }
+  });
+
+  // Close Modal Form esc
+  $(window).on('keydown', function(e) {
+    if (e.keyCode === 27) {
+      if ($('.modal').hasClass('is-active')) {
+        $('.modal').removeClass('is-active');
+        $('.form-for-me').removeClass('is-form-show');
+        $('.form-for-friend').removeClass('is-form-show');
+        $('.form-for-me input').removeClass('is-danger');
+        $('.form-for-friend input').removeClass('is-danger');
+
+      }
+    }
+  });
+
+  /// Validate form ///
+
+  // Form for me
+  $('.form-for-me .modal-form').on('submit', function(e) {
+    var name = document.querySelector('.form-for-me [name=name]');
+    var phone = document.querySelector('.form-for-me [name=tel]');
+    var checkbox = document.querySelector('.form-for-me [name=checkbox]');
+
+    if (!name.value || !phone.value || !checkbox.checked) {
+      e.preventDefault();
+      $('.form-for-me .modal-form').addClass('modal-error');
+    }
+  });
+
+  // Form for friend
+  $('.form-for-friend .modal-form').on('submit', function(e) {
+    var name = document.querySelector('.form-for-friend [name=name]');
+    var phone = document.querySelector('.form-for-friend [name=tel]');
+    var yourName = document.querySelector('.form-for-friend [name=your_name]');
+    var checkbox = document.querySelector('.form-for-friend [name=checkbox]');
+
+    if (!name.value || !phone.value || !yourName.value || !checkbox.checked) {
+      e.preventDefault();
+      $('.form-for-friend .modal-form').addClass('modal-error');
+    }
+  });
+
+  // Customer form
+  $('.customer-form').on('submit', function(e) {
+    var name = document.querySelector('.customer-form [name=name]');
+    var phone = document.querySelector('.customer-form [name=tel]');
+    var checkbox = document.querySelector('.customer-form [name=checkbox]');
+
+    if (!name.value || !phone.value || !checkbox.checked) {
+      e.preventDefault();
+      $('.customer-form').addClass('modal-error');
+    }
   });
 });
